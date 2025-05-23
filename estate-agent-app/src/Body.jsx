d1e1a1e122c2535dcfa2b23f14be03e9d248c6ee
Vishal Oshada <vishaloshada@Vishals-MacBook-Air.local>
2025-05-23 13:16:34 +0530

    new design

--- estate-agent-app/src/Body.jsx
+++ estate-agent-app/src/Body.jsx
@@ -1,29 +1,25 @@
-import '../src/Body.css';
+import './Body.css';
+
 function Body() {
-    return (
-        <div className="body">
-            <div className="left-side">
-                <div className="left-side-content">
-                    <h3>Looking for the perfect home?</h3>
-                    <p>
-                        Your journey to the perfect property starts here.
-                        Whether you're buying your first home,
-                        upgrading your lifestyle, or investing in real estate,
-                        we're here to guide you every step of the way.
-
-                        Discover listings tailored to your goals,
-                        insights from industry experts,
-                        and a seamless experience designed with you in mind.
-
-                        At Home Lands, your future begins with the right foundation.
-                        Let’s find your place together.
-                    </p>
-                    <button className="searchBtn">Search</button>
-                </div>
-            </div>
-        </div>
-
-      
-    )
+  return (
+    <div className="body">
+      <div className="left-side-content">
+        <h3>Looking for the perfect home?</h3>
+        <p>
+          Your journey to the perfect property starts here.
+          Whether you're buying your first home, upgrading your lifestyle,
+          or investing in real estate, we're here to guide you every step of the way.
+          <br /><br />
+          Discover listings tailored to your goals, insights from industry experts,
+          and a seamless experience designed with you in mind.
+          <br /><br />
+          At Home Lands, your future begins with the right foundation.
+          Let’s find your place together.
+        </p>
+        <button className="searchBtn">Search</button>
+      </div>
+    </div>
+  );
 }
+
 export default Body;
--- estate-agent-app/src/Nav.jsx
+++ estate-agent-app/src/Nav.jsx
@@ -1,18 +1,23 @@
-import '../src/Nav.css';
+import './Nav.css';
+
 function NavBar() {
-    return (
-        <nav className="navbar">
-        <div className="navbar-logo">
-            <img src="https://png.pngtree.com/png-clipart/20231219/original/pngtree-house-icon-from-commerce-set-residence-photo-png-image_13880964.png"
-                className="logoImage"></img>
-            <h1>Home Lands</h1>
-        </div>
-        <ul className="navbar-links">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About Us</a></li>
-            <li><a href="#contact">Contact</a></li>
-        </ul>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img
+          src="https://png.pngtree.com/png-clipart/20231219/original/pngtree-house-icon-from-commerce-set-residence-photo-png-image_13880964.png"
+          alt="logo"
+          className="logoImage"
+        />
+        <h1>Home Lands</h1>
+      </div>
+      <ul className="navbar-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About Us</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
 }
+
 export default NavBar;
