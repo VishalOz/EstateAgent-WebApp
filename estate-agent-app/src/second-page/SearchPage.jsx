5ade14428016a31feea448561d129d63130e10d4
Vishal Oshada <vishaloshada@Vishals-MacBook-Air.local>
2025-05-28 23:58:13 +0530

    bug fix

---
+++ estate-agent-app/src/second-page/SearchPage.jsx
@@ -0,0 +1,13 @@
+function searchPage() {
+    return (
+        <div className="contenty">
+            <p>this is where the button directs.</p>
+            <p>this is where the button directs.</p>
+            <p>this is where the button directs.</p>
+            <p>this is where the button directs.</p>
+            <p>this is where the button directs.</p>
+            <p>this is where the button directs.</p>
+        </div>
+    )
+}
+export default searchPage;
