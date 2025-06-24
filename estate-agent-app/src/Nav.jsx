e64a41f37347719e619f01973c069a70d05e0289
VishalOz <vishal.20240036@iit.ac.lk>
2025-06-24 08:16:17 +0530

    removed the search bar

--- estate-agent-app/src/Nav.jsx
+++ estate-agent-app/src/Nav.jsx
@@ -15,14 +15,6 @@ const Nav = () => {
             Contact
           </a>
         </nav>
-        <div className="flex gap-3">
-          <input
-            type="text"
-            placeholder="Search..."
-            className="px-4 py-2 text-white border border-gray-300 rounded-xl focus:outline-none focus:ring-2
-             focus:ring-yellow-400 transition-colors duration-200"
-          />
-        </div>
         <div className="flex items-center">
           <img src="/assets/logo.jpg" alt="logo" className="w-12 h-12 rounded-md" />
           <h3 className="ml-2 font-bold text-white">Home Lands</h3>
