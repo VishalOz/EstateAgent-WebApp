1824a5dd6e9839bf5420426c19e474bc396d0680
VishalOz <vishal.20240036@iit.ac.lk>
2025-06-24 09:16:10 +0530

    search structure

--- estate-agent-app/src/pages/SearchPage.jsx
+++ estate-agent-app/src/pages/SearchPage.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>
-      <h1>Hutta</h1>
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center p-5">
+      <div className="flex gap-8">
+        <div className="w-250 h-auto bg-white shadow-lg rounded p-4">
+
+        </div>
+        <div className="w-64 h-auto bg-white shadow-lg rounded p-4">
+
+        </div>
+      </div>
     </div>
   )
 }
